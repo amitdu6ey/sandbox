802d8124baf8eca9578974206bde3060cc2b1b1f
Amit Dubey <35802447+amitdu6ey@users.noreply.github.com>
2022-03-10 16:18:27 +0530

    Initial commit

---
+++ src/demo/init_logic.js
@@ -0,0 +1,29 @@
+const value = {
+  and: [
+    {
+      "==": [
+        {
+          var: "user.login"
+        },
+        "batman"
+      ]
+    },
+    {
+      "==": [
+        {
+          var: "stock"
+        },
+        false
+      ]
+    },
+    {
+      "==": [
+        {
+          var: "slider"
+        },
+        35
+      ]
+    }
+  ]
+};
+export default value;
